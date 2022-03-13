803d68eccfb6c9b9490b6561f7c2768686a8d915
Nuwanjith Malaka <nuwanjithm@gmail.com>
2022-03-13 13:48:54 +0530

    changed debug to true

--- qbooknew/settings/production.py
+++ qbooknew/settings/production.py
@@ -3,7 +3,7 @@ from .common import *
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['quebooknew2.herokuapp.com']
 

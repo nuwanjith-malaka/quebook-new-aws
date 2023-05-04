760bc0aeb1553a120a3d072dbe46331126dfd7df
nuwanjith-malaka <nuwanjithm@gmail.com>
2023-05-04 12:55:12 +0530

    changed venv creation line

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -2,7 +2,7 @@
 
 # Install libaries
 cd /home/ubuntu/quebook-new-aws
-virtualenv -p python3 venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate

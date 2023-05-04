f2e2deaaf80a2ee3419cf40144131818c6b7320b
nuwanjith-malaka <nuwanjithm@gmail.com>
2023-05-04 10:40:37 +0530

    add a dependancy

--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y python3 python-dev python3-pip apache2 libapache2-mod-wsgi-py3
+sudo apt-get install -y python3 python-dev python3-pip python3-venv apache2 libapache2-mod-wsgi-py3
 pip install --ubuntu --upgrade virtualenv
 sudo rm -rf /home/ubuntu/quebook-new-aws

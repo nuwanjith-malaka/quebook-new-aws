28738ca7689e3bb178885b675cacc69a028781ff
nuwanjith-malaka <nuwanjithm@gmail.com>
2023-05-04 13:07:55 +0530

    changed ubuntu user to root user in appspec file

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo -s
+
 # Install libaries
 cd /home/ubuntu/quebook-new-aws
 python3 -m venv venv
@@ -9,7 +9,7 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 
 # Set permission for all files
-chown -R www-data:www-data /home/ubuntu/quebook-new-aws
+sudo chown -R www-data:www-data /home/ubuntu/quebook-new-aws
 
 # Restart services
-service apache2 restart
+sudo service apache2 restart

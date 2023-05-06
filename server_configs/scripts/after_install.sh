b2234911008df91dbec6524d2e6e6e33c9686ebd
nuwanjith-malaka <nuwanjithm@gmail.com>
2023-05-06 09:42:34 +0530

    changed apache conigs

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -10,6 +10,7 @@ python manage.py collectstatic --no-input
 
 # Set permission for all files
 sudo chown -R www-data:www-data /home/ubuntu/quebook-new-aws
+sudo chmod 755 /home/ubuntu
 
 # Restart services
 sudo service apache2 restart

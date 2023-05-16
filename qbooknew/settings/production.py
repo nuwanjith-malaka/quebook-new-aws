e5345b017c9592ee540fb43f9fe46834cd2f5393
nuwanjith-malaka <nuwanjithm@gmail.com>
2023-05-16 15:24:12 +0530

    added public dns to allowed host

--- qbooknew/settings/production.py
+++ qbooknew/settings/production.py
@@ -5,8 +5,10 @@ DEBUG = True
 import requests
 IMDSv2_TOKEN = requests.put('http://169.254.169.254/latest/api/token', headers={'X-aws-ec2-metadata-token-ttl-seconds': '3600'}).text
 EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01, headers={'X-aws-ec2-metadata-token': IMDSv2_TOKEN}).text
+EC2_PUBLIC_HOSTNAME = requests.get('http://169.254.169.254/latest/meta-data/public-hostname', timeout=0.01, headers={'X-aws-ec2-metadata-token': IMDSv2_TOKEN}).text
+
 ELB_DNS = 'Quebook-test-web-elb-315417778.us-east-1.elb.amazonaws.com'
-ALLOWED_HOSTS = [EC2_PRIVATE_IP, ELB_DNS]
+ALLOWED_HOSTS = [EC2_PRIVATE_IP, ELB_DNS, EC2_PUBLIC_HOSTNAME]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
